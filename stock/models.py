ec16148cd9fdebbff7ebc66b97dca9975138ccf8
Anisur Rahman Mahmud <19202103107@cse.bubt.edu.bd>
2024-03-12 11:58:00 +0600

    some changes

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class stock(models.Model):
+    date = models.DateField()
+    trade_code = models.CharField(max_length=100)
+    high = models.DecimalField(max_digits=4, decimal_places=2)
+    low = models.DecimalField(max_digits=4, decimal_places=2)
+    open = models.DecimalField(max_digits=4, decimal_places=2)
+    close = models.DecimalField(max_digits=4, decimal_places=2)
+    volume = models.IntegerField()
+
--- stockMarket/urls.py
+++ stockMarket/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from stock import views
+
 urlpatterns = [
+    path('', views.homepage_view, name='stock'),
     path('admin/', admin.site.urls),
+    
 ]
